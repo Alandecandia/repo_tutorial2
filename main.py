c6b1c7cea20cf2df1bf8da34e5b2a2e72a8d9964
Alandecandia <decandiaalan08@gmail.com>
2026-04-29 00:29:24 -0300

    Update main.py
    cambiio en main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("hola mundo desde auxiiar!!")
+print("hola mundo desde main!!")
 
-nombre = input("bienvenido, ingrese su nombre: ")
-print(f"hola{nombre}")
+numero = int(input("ingrese un numero: "))
+numero_2 = int(input("ingrese otro nro: "))
+
+print(f"el resultado de la suma es {numero + numero_2} ")
